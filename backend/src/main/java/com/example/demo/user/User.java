a2fbd734e236c62c0a8c04903c9ac7154917a14a
Rene Ruprecht <rene.ruprecht.396@gmail.com>
2022-12-29 20:04:24 +0100

    Add User.java as a simple model

---
+++ backend/src/main/java/com/example/demo/user/User.java
@@ -0,0 +1,28 @@
+package com.example.demo.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @SequenceGenerator(name = "user_id_sequence", sequenceName = "user_id_sequence")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_id_sequence")
+    @Setter(AccessLevel.NONE)
+    private long id;
+    private String name;
+    private String password;
+
+}
